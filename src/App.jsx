e94b8156cbc4dcc0874979bbcb4d249fe04768c4
Srushti Shah <srushtishah@Srushtis-Laptop.local>
2024-09-17 14:44:52 -0400

    Import error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Content from './components/content.jsx'
+import Content from './components/Content.jsx'
 import Footer from './components/Footer.jsx'
 import Home from './components/Home.jsx'
 import Services from './components/Services.jsx'
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
 
                   <div className="contact">
                    <p> For more information please contact: </p>
-                   <p>11, Port Users&apos Complex, Opp. Customs House,Pipavav Port,</p> 
+                   <p>11, Port Users&apos; Complex, Opp. Customs House,Pipavav Port,</p> 
                    <p>Tal:Rajula - 36 55 60 Dist : Amereli Gujarat, IN.</p> 
                    <p> Tel : 02794 - 286 023 • Mob : +91 93772 11223</p> 
                    <li> Email : chiragshah1612@yahoo.com • Skype : chiragshah1612</li> 
